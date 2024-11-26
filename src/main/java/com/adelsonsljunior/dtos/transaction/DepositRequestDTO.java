ede9e66514dbbecf92473d01be67fda684ad124e
adelsonsljunior <junioradelsonst@gmail.com>
2024-11-25 23:02:36 -0300

    ♻️ renaming TransactionRequestDTO to DepositRequestDTO

--- src/main/java/com/adelsonsljunior/dtos/transaction/DepositRequestDTO.java
+++ src/main/java/com/adelsonsljunior/dtos/transaction/DepositRequestDTO.java
@@ -2,7 +2,7 @@ package com.adelsonsljunior.dtos.transaction;
 
 import java.time.LocalDate;
 
-public record TransactionRequestDTO(
+public record DepositRequestDTO(
                 Long userId,
                 double value,
                 LocalDate date) {
