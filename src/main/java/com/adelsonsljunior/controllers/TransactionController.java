be034e354292d1e83d8a4eea3113d0eec82bd7cc
adelsonsljunior <junioradelsonst@gmail.com>
2024-11-27 16:49:53 -0300

    📝 swagger ui

--- src/main/java/com/adelsonsljunior/controllers/TransactionController.java
+++ src/main/java/com/adelsonsljunior/controllers/TransactionController.java
@@ -1,6 +1,7 @@
 package com.adelsonsljunior.controllers;
 
 import com.adelsonsljunior.dtos.transaction.TransactionResponseDTO;
+import com.adelsonsljunior.exceptions.ErrorResponse;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -19,9 +20,18 @@ import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.enums.SchemaType;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
+
 @Path(value = "/transactions")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
+@Tag(name = "Transaction", description = "Operations related to Transactions")
 public class TransactionController {
 
     @Inject
@@ -29,6 +39,11 @@ public class TransactionController {
 
     @POST()
     @Path("/deposit")
+    @Operation(summary = "Create a new transaction", description = "Create a new transaction")
+    @APIResponses(value = {
+            @APIResponse(responseCode = "201", description = "Successful deposit", content = @Content(mediaType = MediaType.APPLICATION_JSON, schema = @Schema(implementation = DepositRequestDTO.class))),
+            @APIResponse(responseCode = "404", description = "User not found", content = @Content(mediaType = MediaType.APPLICATION_JSON, schema = @Schema(implementation = ErrorResponse.class)))
+    })
     public Response deposit(DepositRequestDTO data) {
 
         TransactionResponseDTO createdTransaction = transactionService.deposit(data);
@@ -39,6 +54,12 @@ public class TransactionController {
 
     @GET()
     @Path("/{userId}")
+    @Operation(summary = "List a user's transactions", description = "Returns all transactions of a user")
+    @APIResponses(value = {
+            @APIResponse(responseCode = "200", description = "Returns a list of transactions successfully", content = @Content(mediaType = MediaType.APPLICATION_JSON, schema = @Schema(type = SchemaType.ARRAY, implementation = TransactionResponseDTO.class))),
+            @APIResponse(responseCode = "404", description = "User not found", content = @Content(mediaType = MediaType.APPLICATION_JSON, schema = @Schema(implementation = ErrorResponse.class)))
+
+    })
     public Response findAllByUserId(@PathParam("userId") Long userId) {
         List<TransactionResponseDTO> transactions = transactionService.findAllByUserId(userId);
         return Response.status(Response.Status.OK).entity(transactions).build();
@@ -46,6 +67,11 @@ public class TransactionController {
 
     @GET()
     @Path("/balance/{transactionId}/{currentDate}")
+    @Operation(summary = "Current value of the yield", description = "Returns the current value of the yield")
+    @APIResponses(value = {
+            @APIResponse(responseCode = "200", description = "Returns the curret value successfully", content = @Content(mediaType = MediaType.APPLICATION_JSON, schema = @Schema(implementation = BalanceResponseDTO.class))),
+            @APIResponse(responseCode = "404", description = "Transaction not found", content = @Content(mediaType = MediaType.APPLICATION_JSON, schema = @Schema(implementation = ErrorResponse.class)))
+    })
     public Response getBalance(@PathParam("transactionId") Long transactionId,
             @PathParam("currentDate") LocalDate currentDate) {
 
--- src/main/java/com/adelsonsljunior/controllers/UserController.java
+++ src/main/java/com/adelsonsljunior/controllers/UserController.java
@@ -1,5 +1,11 @@
 package com.adelsonsljunior.controllers;
 
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+
 import com.adelsonsljunior.dtos.user.UserRequestDTO;
 import com.adelsonsljunior.dtos.user.UserResponseDTO;
 import com.adelsonsljunior.services.UserService;
@@ -15,12 +21,16 @@ import jakarta.ws.rs.core.Response;
 @Path(value = "/users")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
+@Tag(name = "User", description = "Operations related to Users")
 public class UserController {
 
     @Inject
     UserService userService;
 
     @POST
+    @Operation(summary = "Create a new user")
+    @APIResponse(responseCode = "201", description = "User created successfully", content = @Content(mediaType = MediaType.APPLICATION_JSON, schema = @Schema(implementation = UserResponseDTO.class)))
+
     public Response create(UserRequestDTO data) {
 
         UserResponseDTO createdUser = userService.create(data);
